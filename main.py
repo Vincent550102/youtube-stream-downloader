0ce386bb146f64b0a5cd8980da5ded981fdd4cf0
Vincent550102 <5020559@gmail.com>
2024-02-19 15:42:03 +0800

    feature yt-dlp

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import subprocess
 output_format = r'outputs\%(upload_date)s_%(title)s'
 ytarchive_bin = r'bin\ytarchive.exe'
 ffmpeg_bin = r'bin\ffmpeg.exe'
+yt_dlp_bin = r'bin\yt-dlp.exe'
 
 def record_live_stream(url):
-    result = subprocess.run([ytarchive_bin, '-w', '--merge', '-o', output_format, '--ffmpeg-path', ffmpeg_bin, url, 'best'])
+    result = subprocess.run([ytarchive_bin, '-w', '--merge', '-o', output_format, '--ffmpeg-path', ffmpeg_bin, url, 'best'], capture_output=True)
+    if "Use yt-dlp instead." in result.stderr.decode('utf-8'):
+        subprocess.run([yt_dlp_bin, '-o', output_format, '--ffmpeg-location', ffmpeg_bin, url])
     return result
 
 # 直播連結列表
@@ -21,6 +24,7 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     for future in concurrent.futures.as_completed(futures):
         try:
             result = future.result()
+            print(result)
         except Exception as exc:
             print(f'錄製任務產生了一個異常: {exc}')
         else:
